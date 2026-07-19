31d3be32f926275a36b22dd8bed652c234ef6f9d
yukuan95 <zhaoyukuan95@gmail.com>
2026-07-19 12:53:52 +0900

    .

--- react-test/src/PageComp/Table.tsx
+++ react-test/src/PageComp/Table.tsx
@@ -1,11 +1,11 @@
 import { Tooltip } from '../Components/Tooltip'
 import { state as _state, } from '../Store'
 import { useProxy } from 'valtio/utils'
+import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { css } from '@emotion/css'
 import * as lib from '../Lib'
 import { Table } from 'antd'
-import React from 'react'
 const { Column } = Table
 
 const heightCss = css`height: 30px;`
@@ -47,11 +47,12 @@ export const Table1 = () => {
 
 export const Table2 = () => {
   const state = useProxy(_state)
+  const [hidden, setHidden] = useState(true)
   const orderMonth = state.data?.analyseData?.orderMonth?.find(item => item.month === state.yearMonth)?.orderMonthArray ?? []
   const data: any = orderMonth.map((item) => {
-    const { time, nowPrice, longPrice, status, preS, longChg, maxMinChg, status2, preS2 } = item
+    const { isStart, time, nowPrice, longPrice, status, preS, longChg, maxMinChg, status2, preS2 } = item
     return {
-      key: uuidv4(), status, status2,
+      key: uuidv4(), status, status2, isStart,
       time, timeShort: time.slice(0, 16),
       nowPrice: lib.formatNumber(nowPrice, 1),
       longPrice: lib.formatNumber(longPrice, 1),
@@ -62,8 +63,11 @@ export const Table2 = () => {
     }
   })
   const timeZone = data.at(0)?.time?.slice(-3) ?? ''
+  const data2 = data.filter((item: any, index: any) => {
+    return !(orderMonth[index - 1]?.isStart === false && item.isStart === false)
+  })
   return <>
-    <Table size="small" pagination={false} bordered dataSource={data} >
+    <Table size="small" pagination={false} bordered dataSource={hidden ? data2 : data} >
       <Column
         align="center" title={timeZone ? `time(${timeZone})` : 'time'}
         className={heightCss} dataIndex="timeShort" render={(_, item) => (<>
@@ -72,13 +76,15 @@ export const Table2 = () => {
       <Column className={heightCss} align="center" title="price" dataIndex="nowPrice" render={(_, item) => (<>
         <Tooltip title={getGrid(item,
           [['longPrice', 'longPrice'], ['longChg', 'longChg'], ['maxChg', 'maxMinChg'],
-          ['status2', 'status2'], ['rate2', 'preS2'],]
+          ['status2', 'status2'], ['rate2', 'preS2']]
         )} placement='left'>
           {item.nowPrice}
         </Tooltip>
       </>)}></Column>
       <Column className={heightCss} align="center" title="status" dataIndex="status"></Column>
-      <Column className={heightCss} align="center" title="rate" dataIndex="preS"></Column>
+      <Column className={heightCss} align="center" title={<>
+        <div className={css`user-select: none;`} onDoubleClick={() => setHidden(!hidden)}>rate</div>
+      </>} dataIndex="preS"></Column>
     </Table >
   </>
 }
@@ -99,12 +105,12 @@ export const Table5 = () => {
 }
 
 function getGrid(item: any, keys: Array<[string, string]>) {
-  keys = keys.filter((key) => !!item[key[1]])
+  keys = keys.filter((key) => item[key[1]] !== null)
   return <>
     <div style={{ display: 'grid', gridTemplateColumns: 'auto auto auto', justifyItems: 'center' }}>
       {
         keys.map((key: [string, string]) => (<React.Fragment key={`${item.key}${key[0]}${key[1]}`}>
-          <div>{key[0]}</div><div style={{ whiteSpace: 'pre' }}> : </div><div>{item[key[1]]}</div>
+          <div>{key[0]}</div><div style={{ whiteSpace: 'pre' }}> : </div><div>{String(item[key[1]])}</div>
         </React.Fragment>))
       }
     </div>
